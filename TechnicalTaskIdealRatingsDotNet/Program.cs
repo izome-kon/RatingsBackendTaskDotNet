e152745b163eb19ff430921229b323b04af03532
Mahmoud Metwally <58386176+izome-kon@users.noreply.github.com>
2025-04-15 20:01:19 +0200

    Refactor comments in Program.cs for clarity on CORS and migration sections

--- TechnicalTaskIdealRatingsDotNet/Program.cs
+++ TechnicalTaskIdealRatingsDotNet/Program.cs
@@ -5,7 +5,7 @@ using TechnicalTaskIdealRatingsDotNet.Repositories.Person;
 using TechnicalTaskIdealRatingsDotNet.Services;
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-
+// Add CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
@@ -25,7 +25,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("mssqlConnection")));
 
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -39,7 +38,7 @@ builder.Services.AddScoped<PersonService>();
 
 var app = builder.Build();
 
-
+// Apply migrations
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
